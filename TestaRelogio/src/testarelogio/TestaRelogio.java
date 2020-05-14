618aade90b6728ee766a68f7b6673a72bc022d03
FELIPPE RODRIGUES DOS SANTOS <fe-santos00@hotmail.com>
2020-05-13 23:35:07 -0300

    adicionando TestaRelogio

---
+++ TestaRelogio/src/testarelogio/TestaRelogio.java
@@ -0,0 +1,14 @@
+package testarelogio;
+public class TestaRelogio {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Relogio hora1 = new Relogio();
+        hora1.adicionarHora();
+        hora1.mostrarHora();
+        hora1.adicionarHora();
+        hora1.mostrarHora();
+        hora1.adicionarHora();
+        hora1.mostrarHora();
+    }
+    
+}
