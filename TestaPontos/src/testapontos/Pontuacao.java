3b1fadc730216cec027ad352d2e146b80e6a1bd1
FELIPPE RODRIGUES DOS SANTOS <fe-santos00@hotmail.com>
2020-05-13 22:59:41 -0300

    master

---
+++ TestaPontos/src/testapontos/Pontuacao.java
@@ -0,0 +1,24 @@
+package testapontos;
+public class Pontuacao {
+        //Atributos
+        int pontosGanhos;
+    public void Pontuacao(){
+        
+    }
+    public void ganharPartida(){
+        pontosGanhos += 6;
+        System.out.println("Você ganhou 6 pontos");
+    }
+    public void empatarPartida(){
+        pontosGanhos += 3;
+        System.out.println("Você empatou e ganhou 3 pontos");
+    }
+    public void perderPartida(){
+        pontosGanhos += 0;
+        System.out.println("Você perdeu e não ganhou pontos");
+    }
+    public void mostrarPontuacao(){
+        System.out.println("Sua pontuação atual é: " + pontosGanhos);
+    }
+}
+
