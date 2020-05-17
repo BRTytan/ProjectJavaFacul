2fb7c2c3fb0cc0d4c1514ba9b0229d4161747c0a
FELIPPE RODRIGUES DOS SANTOS <fe-santos00@hotmail.com>
2020-05-17 15:36:15 -0300

    up

--- TestaOrdem/src/testaordem/Ordem.java
+++ TestaOrdem/src/testaordem/Ordem.java
@@ -1,55 +1,69 @@
 package testaordem;
 
-import static java.util.Arrays.sort;
-import static java.util.Arrays.sort;
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import static java.util.Collections.list;
+import java.util.List;
 
 public class Ordem {
     //Atributos
     int x,y,z;
-    public void Ordem(int a, int b, int c){
-        x = a;
-        y = b;
-        z = c;
-        /*x = Integer.parseInt(JOptionPane.showInputDialog(a));
-        y = Integer.parseInt(JOptionPane.showInputDialog(b));
-        z = Integer.parseInt(JOptionPane.showInputDialog(c));
-        */
+    public Ordem(int a, int b, int c){
+        
+        int [] list1 = {15, 10, 25};
+        x=list1[0];
+        y=list1[1];
+        z=list1[2];
     }
     public void mostrarMaior(){
         if(x > y && x > z){
+            System.out.println("Mostrar Maior: ");
             System.out.println("X"+ "(" + x + ")" + " > " + y + " e " + z);
+            System.out.println("X é > que Y e Z");
+            System.out.println("---------------------------");
         }
         else if(y > x && y > z){
+            System.out.println("Mostrar Maior: ");
             System.out.println("Y"+ "(" + y + ")" + " > " + x + " e " + z);
+            System.out.println("Y é > que X e Z");
+            System.out.println("---------------------------");
         }
         else{
+            System.out.println("Mostrar Maior: ");
             System.out.println("Z"+ "(" + z + ")" + " > " + x + " e " + y);
+            System.out.println("Z é > que X e Y");
+            System.out.println("---------------------------");
         }
     }
     public void mostrarMenor(){
         if(x < y && x < z){
+            System.out.println("Mostrar Menor: ");
             System.out.println("X"+ "(" + x + ")" + " < " + y + " e " + z);
+            System.out.println("X é < que Y e Z");
+            System.out.println("---------------------------");
         }
         else if(y < x && y < z){
+            System.out.println("Mostrar Menor: ");
             System.out.println("Y"+ "(" + y + ")" + " < " + x + " e " + z);
+            System.out.println("Y é < que X e Z");
+            System.out.println("---------------------------");
         }
         else{
+            System.out.println("Mostrar Menor: ");
             System.out.println("Z"+ "(" + z + ")" + " < " + x + " e " + y);
+            System.out.println("Z é < que X e Y");
+            System.out.println("---------------------------");
         }
     }
     public void mostrarCrescente(){
-        int[] list1 = {x,y,z};
-        System.out.println(list1);
-        if(x < y && x < z){
-            System.out.println("X"+ "(" + x + ")" + " < " + y + " e " + z);
-            sort(list1);
-        }
-        else if(y < x && y < z){
-            System.out.println("Y"+ "(" + y + ")" + " < " + x + " e " + z);
-        }
-        else{
-            System.out.println("Z"+ "(" + z + ")" + " < " + x + " e " + y);
-        }
+        int [] list1 = {x,y,z};
+        Arrays.sort(list1);
+            System.out.println("Ordem Crescente: ");
+            System.out.println(Arrays.toString(list1));
+            System.out.println("---------------------------");
+    }
+    public void mostrarDecrescente(){
+       
     }
 }
--- TestaOrdem/src/testaordem/TestaOrdem.java
+++ TestaOrdem/src/testaordem/TestaOrdem.java
@@ -1,21 +1,12 @@
 package testaordem;
+
 public class TestaOrdem {
     public static void main(String[] args) {
         // TODO code application logic here
         //Objetos
-        Ordem ord1 = new Ordem(); 
-        ord1.Ordem(10, 30, 20);
+        Ordem ord1 = new Ordem(20,15,10);
         ord1.mostrarMaior();
         ord1.mostrarMenor();
-        ord1.mostrarCrescente();
-        ord1.Ordem(30, 10, 20);
-        ord1.mostrarMaior();
-        ord1.mostrarMenor();
-        ord1.mostrarCrescente();
-        ord1.Ordem(10, 10, 20);
-        ord1.mostrarMaior();
-        ord1.mostrarMenor();
-        ord1.mostrarCrescente();
+        ord1.mostrarCrescente();        
     }
-    
 }
