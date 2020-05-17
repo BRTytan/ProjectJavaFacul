987ee5b499ee9ccbd82e0df952d64f3a4fdf69e8
FELIPPE RODRIGUES DOS SANTOS <fe-santos00@hotmail.com>
2020-05-17 00:34:49 -0300

    up

--- TestaAutomovel/src/testaautomovel/TestaAutomovel.java
+++ TestaAutomovel/src/testaautomovel/TestaAutomovel.java
@@ -3,10 +3,6 @@ package testaautomovel;
 public class TestaAutomovel {
     public static void main(String[] args) {
         // TODO code application logic here
-        //Atributos
-        boolean ligado,ligado1,ligado2;
-        
-        
         Automovel carro1 = new Automovel();
         Automovel carro2 = new Automovel();
         Automovel carro3 = new Automovel();
@@ -14,6 +10,7 @@ public class TestaAutomovel {
         carro1.Automovel("FIAT","FFJ4045", "PRETO", "ALCOOL", "123456", "DHJIL", true);
         carro2.Automovel("GOL","FFJ4045", "AZUL", "GASOLINA", "123456", "DHJIL", false);
         carro3.Automovel("AUDI","FFJ4045", "PRETO", "ALCOOL", "123456", "DHJIL", true);
+        
+        carro1.ligaDesliga();
     }
-    
 }
